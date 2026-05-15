9ab1643464beca5a69bdaa75a4be686e17e4e6a1
Asadkhon Rasulov <asadbeka09022003@gmail.com>
2026-05-15 12:14:34 +0500

    completed the modal project. learned about componenets, styles, props, slots, etc.

--- Vue_CLI/modal-project/src/App.vue
+++ Vue_CLI/modal-project/src/App.vue
@@ -1,26 +1,84 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>my first project: {{ title }}</h1>
+  <br />
+
+  <input type="text" ref="name" />
+  <button @click="handleClick">click me</button>
+
+  <div v-if="showModal">
+    <Modal theme="" @hide="toggleShowModal">
+      <h1>Modal title</h1>
+      <h2>{{ header2 }}</h2>
+      <p>{{ text }}</p>
+      <p>modal content</p>
+      <span>{{ sampleList }}</span>
+      <template v-slot:links>
+        <a href="">sign up</a>
+        <a href="">more info</a>
+      </template>
+    </Modal>
+  </div>
+  <br />
+
+  <teleport to="#modals" v-if="showModal2">
+    <Modal @hide="toggleShowModal2">
+      <h1>This the second modal</h1>
+      <template v-slot:links>
+        <a href="">sign up</a>
+        <a href="">more info</a>
+      </template>
+    </Modal>
+  </teleport>
+  <br />
+  <button @click.alt="toggleShowModal">show modal 1(alt)</button>
+  <button @click.shift="toggleShowModal2">show modal 2 (shift)</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "This is the title",
+      header2: "This is a prop named 'header2'",
+      text: "This is another prop named text",
+      sampleList: [1, 2, 3],
+      showModal: false,
+      showModal2: true,
+    };
+  },
+  methods: {
+    handleClick() {
+      console.log(this.$refs.name);
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleShowModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleShowModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app, #modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
---
+++ Vue_CLI/modal-project/src/components/Modal.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="backdrop" @click.self="hideModal">
+    <div class="modal" :class="{ sale: theme == 'sale' }">
+      <slot>Default slot content</slot>
+      <div class="actions">
+        <slot name="links"></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["theme"],
+  methods: {
+    hideModal() {
+      this.$emit("hide");
+    },
+  },
+};
+</script>
+
+<style>
+.modal {
+  width: 400px;
+  padding: 20px;
+  margin: 100px auto;
+  background: white;
+  border-radius: 10px;
+}
+
+.backdrop {
+  position: fixed;
+  top: 0;
+  background: rgba(0, 0, 0, 0.5);
+  width: 100%;
+  height: 100%;
+}
+
+h1 {
+  color: aquamarine;
+  border: none;
+  padding: 0;
+}
+
+.sale {
+  background-color: crimson;
+  color: white;
+}
+
+.sale h1 {
+  color: white;
+}
+
+.actions {
+  text-align: center;
+  margin: 30px 0 10px 0;
+  color: #333;
+}
+
+.modal .actions a {
+  color: #333;
+  padding: 8px;
+  border: 1px solid #eee;
+  border-radius: 4px;
+  text-decoration: none;
+  margin: 10px;
+}
+</style>
